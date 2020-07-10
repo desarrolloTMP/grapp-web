07211e7d3ae98d29d204b73b2628179f0400ec81
unknown <CLINICANU-293@CLINICAN-293.clinicadelnorte.local>
2020-07-10 18:07:44 -0500

    cambio variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,7 +6,8 @@ export const environment = {
     appId: '5cdadea643779f38588ffdc2',
     apiKey: 'U2FsdGVkX1/AJwijWF8J3LMdBHHorp5SWkfpeOiOOy4=',
     userId: 'developer+grapp@tresastronautas.com',
-    apiUrl: 'http://68.183.115.58:3200',
+    //apiUrl: 'http://68.183.115.58:3200',
+    apiUrl: 'http://192.168.1.234:3200',
     aesKey: '$4lud3l3ctr0n1C4Gr4pP',
   },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,12 +6,13 @@ export const environment = {
   production: false,
   serverURL: 'http://localhost:3000',
   //serverURL: 'http://192.168.1.231:3000',
-  appURL: 'http://localhost:4214',
+  appURL: 'http://localhost:4200',
   sesso: {
     appId: '5cdadea643779f38588ffdc2',
     apiKey: 'U2FsdGVkX1/AJwijWF8J3LMdBHHorp5SWkfpeOiOOy4=',
     userId: 'developer+grapp@tresastronautas.com',
-    apiUrl: 'http://68.183.115.58:3200',
+    //apiUrl: 'http://68.183.115.58:3200',
+    apiUrl: 'http://192.168.1.234:3200',
     aesKey: '$4lud3l3ctr0n1C4Gr4pP',
   },
 };
